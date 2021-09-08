99ee715d36597edad402cabfeff22fd9702fb5e4
Anthony <bouillant.anthony@gmail.com>
2021-09-08 22:29:17 +0200

    Fix frontend visual errors

--- templates/answering/index.html.php
+++ templates/answering/index.html.php
@@ -1,8 +1,7 @@
 <header class="heading answering">
-    <section class="container">
-        <a href="/"><img src="/assets/logo-84d7d70645fbe179ce04c983a5fae1e6cba523d7cd28e0cd49a04707ccbef56e.png" /></a>
-
-    </section>
+    <?php
+    include_once('./templates/_header.html.php');
+    ?>
 </header>
 
 <main class="container">
