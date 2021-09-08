e2777703753b3e3e50aaf92f2564f6ecf2eb4add
Anthony <bouillant.anthony@gmail.com>
2021-09-08 22:29:17 +0200

    fix frontend details

--- templates/base.html.php
+++ templates/base.html.php
@@ -3,16 +3,10 @@
 
 <head>
     <title>ExerciseLooper</title>
-    <link rel="stylesheet" media="all" href="/style.css" />
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
-
-    <script src="/assets/application-212289bcba525f2374cdbd70755ea38f2cfdd35d479e9638fae0b2832fac5dac.js"></script>
+    <link rel="stylesheet" media="all" href="assets/style.css" />
+    <script src="assets/script.js"></script>
 </head>
 
-
-
 <?= $pageContent ?>
-
-
 
 </html>
