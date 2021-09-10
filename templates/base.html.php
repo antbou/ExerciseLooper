7242c07d986f1445029ea990f3208ac9b81c1a2d
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2021-09-10 11:25:44 +0200

    Implementation of responsive design

--- templates/base.html.php
+++ templates/base.html.php
@@ -3,7 +3,9 @@
 
 <head>
     <title>ExerciseLooper</title>
-    <link rel="stylesheet" media="all" href="assets/style.css" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" media="all" href="assets/style/style.css" />
     <script src="assets/script.js"></script>
 </head>
 
