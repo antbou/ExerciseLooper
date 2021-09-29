31abada8f34f8e865c3b6a653eaace5fe25fbde4
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2021-09-29 13:59:51 +0200

    Update direct access to public files

--- public/index.php
+++ public/index.php
@@ -1,39 +1,7 @@
 <?php
-require_once('core/Render.php');
+require_once('../core/services/Router.php');
 
-$variable = null;
+$router = new Router($_SERVER['REQUEST_URI']);
+$router->setConfig();
 
-// évite les messages d'erreurs type "warning" lorsque $_GET['page'] n'existe pas
-if (!isset($_GET['page'])) {
-    $_GET['page'] = "";
-}
-
-
-// obtient chaque arguments de l'URL
-$settings = explode('/', $_GET['page']);
-
-if ($settings[0] != "") {
-    $controller = $settings[0];
-
-    // définit l'action du controller exercises
-    $action = isset($settings[1]) ? $settings[1] : 'home';
-
-    switch ($action) {
-        case 'answering':
-            Render::render('answering/index');
-            break;
-        case 'new':
-            Render::render('new/index');
-            break;
-        case 'home':
-            Render::render('exercises/index');
-            break;
-        default:
-            http_response_code(404);
-            include('templates/errors/404.html.php');
-            die;
-            break;
-    }
-} else {
-    Render::render('home/index');
-}
+$router->process();
