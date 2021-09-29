f298064979a1eabbb11d4065c1146e683593a1a8
BOUILLANT Anthony <Anthony.BOUILLANT@cpnv.ch>
2021-09-29 15:42:49 +0200

    Test Route ok

---
+++ tests/RouteTest.php
@@ -0,0 +1,45 @@
+<?php
+
+require('core/service/Route.php');
+
+use PHPUnit\Framework\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * @covers $team->doesMatch()
+     */
+    public function testDoesMatch()
+    {
+        $path = '/test/:id';
+        $controllerName = 'test';
+        $method = 'test';
+        $route = new Route($path, $controllerName, $method);
+
+        $urlTrue = '/test/18';
+        $urlFalse = '/test/18/test';
+
+
+        $this->assertTrue($route->doesMatch($urlTrue));
+        $this->assertFalse($route->doesMatch($urlFalse));
+    }
+
+    /**
+     * @covers $team->generateUrl()
+     */
+    public function testGenerateUrl()
+    {
+        $path = '/test/:id';
+        $controllerName = 'test';
+        $method = 'test';
+
+        $param = ['id' => 18];
+
+        $route = new Route($path, $controllerName, $method);
+        $urlTrue = 'test/18';
+        $urlFalse = 'test/false';
+
+        $this->assertSame($urlTrue, $route->generateUrl($param));
+        $this->assertNotSame($urlFalse, $route->generateUrl($param));
+    }
+}
