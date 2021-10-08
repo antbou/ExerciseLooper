05ce1583a9c68c21833cf17e0d29942d092ce3aa
DELGADO Noah <Noah.DELGADO@cpnv.ch>
2021-10-08 10:23:59 +0200

    Add view for new exercise

---
+++ controllers/CreateExerciseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Looper\controllers;
+
+use Looper\core\services\Http;
+
+class CreateExerciseController
+{
+    public function show()
+    {
+        Http::response('new/index');
+    }
+}
