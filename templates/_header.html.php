c526e2fb1bf9466e8933daf901cf02b0ce59af54
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-11-05 09:20:53 +0100

    Fix question view

--- templates/_header.html.php
+++ templates/_header.html.php
@@ -1,4 +1,4 @@
 <section class="container">
     <a href=<?= $router->getUrl('HomePage', []); ?>><img src="/resources/logo.png" /></a>
-    <span class="exercise-label"><?= (isset($exercise) ? $exercise->getPublicName() : Looper\models\Exercise::DEFAULTNAME) ?></span>
+    <span class="exercise-label"><?= (isset($exercise) ? $exercise->getPublicName() : $title) ?></span>
 </section>
