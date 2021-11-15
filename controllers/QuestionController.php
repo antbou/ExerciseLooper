8de537d1ef8db3fbce2c17986cf839d12f521df6
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-11-15 10:50:52 +0100

    Refactoring QuestionController

--- controllers/QuestionController.php
+++ controllers/QuestionController.php
@@ -15,33 +15,25 @@ class QuestionController extends AbstractController
 {
     public function create(int $id)
     {
-
         $exercise = Repository::find($id, Exercise::class);
 
-        if (empty($exercise)) {
-            Http::notFoundException();
-        }
+        if (empty($exercise)) return Http::notFoundException();
 
         $form = new FormValidator('field');
-        $form
-            ->addField(['value' => new Field('label', 'string', true)])
-            ->addField(['valueKind' => new Field('value_kind', 'string', false, valueToVerify: $this->getConstants(QuestionState::class, true))]);;
-
+        $form->addField(['value' => new Field('label', 'string', true)])
+            ->addField(['valueKind' => new Field('value_kind', 'string', false, valueToVerify: $this->getConstants(QuestionState::class, true))]);
 
         if ($form->process() && $this->csrfValidator()) {
-
             $question = Question::make([
                 'value' => $form->getFields()['value']->value,
                 'valueKind' => QuestionState::getConstValue($form->getFields()['valueKind']->value),
                 'exercises_id' => $exercise->getId()
             ]);
 
-            if ($question->create()) {
-                Http::redirectToRoute('CreateQuestion', ['idExercise' => $exercise->getId()]);
-            }
+            if ($question->create()) return Http::redirectToRoute('CreateQuestion', ['idExercise' => $exercise->getId()]);
         }
 
-        Http::response('new/question', ['exercise' => $exercise], hasForm: true);
+        return Http::response('new/question', ['exercise' => $exercise], hasForm: true);
     }
 
     public function delete(int $idExercise, int $idQuestion): bool
