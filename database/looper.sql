b8a351977b2e094da193855eac4b0e6dea219224
Anthony <anthony.bouillant@cpnv.ch>
2021-11-23 17:50:06 +0100

    Update sql

--- database/looper.sql
+++ database/looper.sql
@@ -1,125 +1,156 @@
--- --------------------------------------------------------
--- Hôte:                         127.0.0.1
--- Version du serveur:           8.0.26 - MySQL Community Server - GPL
--- SE du serveur:                Linux
--- HeidiSQL Version:             11.3.0.6295
--- --------------------------------------------------------
+-- MySQL Workbench Forward Engineering
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
+-- -----------------------------------------------------
+-- Schema db_exerciselooper
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `db_exerciselooper` ;
 
--- Listage de la structure de la base pour db_exerciselooper
-CREATE DATABASE IF NOT EXISTS `db_exerciselooper` /*!40100 DEFAULT CHARACTER SET utf8 */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `db_exerciselooper`;
+-- -----------------------------------------------------
+-- Schema db_exerciselooper
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `db_exerciselooper` DEFAULT CHARACTER SET utf8 ;
+USE `db_exerciselooper` ;
 
--- Listage de la structure de la table db_exerciselooper. exercises
-CREATE TABLE IF NOT EXISTS `exercises` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `title` varchar(45) NOT NULL,
-  `status_id` int NOT NULL,
+-- -----------------------------------------------------
+-- Table `db_exerciselooper`.`status`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_exerciselooper`.`status` ;
+
+CREATE TABLE IF NOT EXISTS `db_exerciselooper`.`status` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `value` VARCHAR(45) NOT NULL,
+  `slug` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_exercises_status1_idx` (`status_id`),
-  CONSTRAINT `fk_exercises_status1` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  UNIQUE INDEX `slug_UNIQUE` (`slug` ASC) VISIBLE)
+ENGINE = InnoDB;
 
--- Listage des données de la table db_exerciselooper.exercises : ~0 rows (environ)
-DELETE FROM `exercises`;
-/*!40000 ALTER TABLE `exercises` DISABLE KEYS */;
-/*!40000 ALTER TABLE `exercises` ENABLE KEYS */;
 
--- Listage de la structure de la table db_exerciselooper. questions
-CREATE TABLE IF NOT EXISTS `questions` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `value` varchar(45) NOT NULL,
-  `exercise_id` int NOT NULL,
-  `states_id` int NOT NULL,
+-- -----------------------------------------------------
+-- Table `db_exerciselooper`.`exercises`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_exerciselooper`.`exercises` ;
+
+CREATE TABLE IF NOT EXISTS `db_exerciselooper`.`exercises` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `title` VARCHAR(45) NOT NULL,
+  `status_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_fields_exercises_idx` (`exercise_id`),
-  KEY `fk_questions_states1_idx` (`states_id`),
-  CONSTRAINT `fk_fields_exercises` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`),
-  CONSTRAINT `fk_questions_states1` FOREIGN KEY (`states_id`) REFERENCES `states` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  INDEX `fk_exercises_status1_idx` (`status_id` ASC) VISIBLE,
+  CONSTRAINT `fk_exercises_status1`
+    FOREIGN KEY (`status_id`)
+    REFERENCES `db_exerciselooper`.`status` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- Listage des données de la table db_exerciselooper.questions : ~0 rows (environ)
-DELETE FROM `questions`;
-/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
-/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 
--- Listage de la structure de la table db_exerciselooper. responses
-CREATE TABLE IF NOT EXISTS `responses` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `value` varchar(1000) NOT NULL,
-  `question_id` int NOT NULL,
-  `serie_id` int NOT NULL,
+-- -----------------------------------------------------
+-- Table `db_exerciselooper`.`states`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_exerciselooper`.`states` ;
+
+CREATE TABLE IF NOT EXISTS `db_exerciselooper`.`states` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NOT NULL,
+  `slug` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_responses_questions1_idx` (`question_id`),
-  KEY `fk_responses_series1_idx` (`serie_id`),
-  CONSTRAINT `fk_responses_questions1` FOREIGN KEY (`question_id`) REFERENCES `questions` (`id`),
-  CONSTRAINT `fk_responses_series1` FOREIGN KEY (`serie_id`) REFERENCES `series` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  UNIQUE INDEX `slug_UNIQUE` (`slug` ASC) VISIBLE)
+ENGINE = InnoDB;
 
--- Listage des données de la table db_exerciselooper.responses : ~0 rows (environ)
-DELETE FROM `responses`;
-/*!40000 ALTER TABLE `responses` DISABLE KEYS */;
-/*!40000 ALTER TABLE `responses` ENABLE KEYS */;
 
--- Listage de la structure de la table db_exerciselooper. series
-CREATE TABLE IF NOT EXISTS `series` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `date` datetime NOT NULL,
-  `exercise_id` int DEFAULT NULL,
+-- -----------------------------------------------------
+-- Table `db_exerciselooper`.`questions`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_exerciselooper`.`questions` ;
+
+CREATE TABLE IF NOT EXISTS `db_exerciselooper`.`questions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `value` VARCHAR(45) NOT NULL,
+  `exercise_id` INT NOT NULL,
+  `states_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `fk_series_exercises1_idx` (`exercise_id`),
-  CONSTRAINT `fk_series_exercises1` FOREIGN KEY (`exercise_id`) REFERENCES `exercises` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+  INDEX `fk_fields_exercises_idx` (`exercise_id` ASC) VISIBLE,
+  INDEX `fk_questions_states1_idx` (`states_id` ASC) VISIBLE,
+  CONSTRAINT `fk_fields_exercises`
+    FOREIGN KEY (`exercise_id`)
+    REFERENCES `db_exerciselooper`.`exercises` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_questions_states1`
+    FOREIGN KEY (`states_id`)
+    REFERENCES `db_exerciselooper`.`states` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- Listage des données de la table db_exerciselooper.series : ~0 rows (environ)
-DELETE FROM `series`;
-/*!40000 ALTER TABLE `series` DISABLE KEYS */;
-/*!40000 ALTER TABLE `series` ENABLE KEYS */;
 
--- Listage de la structure de la table db_exerciselooper. states
-CREATE TABLE IF NOT EXISTS `states` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `name` varchar(45) NOT NULL,
-  `slug` varchar(45) NOT NULL,
+-- -----------------------------------------------------
+-- Table `db_exerciselooper`.`series`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_exerciselooper`.`series` ;
+
+CREATE TABLE IF NOT EXISTS `db_exerciselooper`.`series` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NOT NULL,
+  `exercise_id` INT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `slug_UNIQUE` (`slug`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
+  INDEX `fk_series_exercises1_idx` (`exercise_id` ASC) VISIBLE,
+  CONSTRAINT `fk_series_exercises1`
+    FOREIGN KEY (`exercise_id`)
+    REFERENCES `db_exerciselooper`.`exercises` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_exerciselooper`.`responses`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `db_exerciselooper`.`responses` ;
+
+CREATE TABLE IF NOT EXISTS `db_exerciselooper`.`responses` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `value` VARCHAR(1000) NOT NULL,
+  `question_id` INT NOT NULL,
+  `serie_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `fk_responses_questions1_idx` (`question_id` ASC) VISIBLE,
+  INDEX `fk_responses_series1_idx` (`serie_id` ASC) VISIBLE,
+  CONSTRAINT `fk_responses_questions1`
+    FOREIGN KEY (`question_id`)
+    REFERENCES `db_exerciselooper`.`questions` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_responses_series1`
+    FOREIGN KEY (`serie_id`)
+    REFERENCES `db_exerciselooper`.`series` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
 
 -- Listage des données de la table db_exerciselooper.states : ~0 rows (environ)
 DELETE FROM `states`;
 /*!40000 ALTER TABLE `states` DISABLE KEYS */;
 INSERT INTO `states` (`id`, `name`, `slug`) VALUES
-	(1, 'single_line', 'SIN'),
-	(2, 'single_line_list', 'LIS'),
-	(3, 'multi_line', 'MUL');
+	(1, 'single_line', 'SIN_LIN'),
+	(2, 'single_line_list', 'SIN_LIS'),
+	(3, 'multi_line', 'MUL_LIN');
 /*!40000 ALTER TABLE `states` ENABLE KEYS */;
-
--- Listage de la structure de la table db_exerciselooper. status
-CREATE TABLE IF NOT EXISTS `status` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `value` varchar(45) NOT NULL,
-  `slug` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `slug_UNIQUE` (`slug`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3;
 
 -- Listage des données de la table db_exerciselooper.status : ~0 rows (environ)
 DELETE FROM `status`;
 /*!40000 ALTER TABLE `status` DISABLE KEYS */;
 INSERT INTO `status` (`id`, `value`, `slug`) VALUES
-	(1, 'underconstruct', 'UND'),
-	(2, 'answered', 'ANS'),
-	(3, 'terminate', 'TER');
-/*!40000 ALTER TABLE `status` ENABLE KEYS */;
-
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+	(1, 'underconstruct', 'UNDE'),
+	(2, 'answered', 'ANSW'),
+	(3, 'terminate', 'TERM');
