f8c81efa0d988a80f1ab297a54827551c99eb34c
DELGADO Noah <Noah.DELGADO@cpnv.ch>
2021-12-06 11:48:22 +0100

    add serieController

---
+++ controllers/SerieController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Looper\controllers;
+
+use Looper\models\Exercise;
+use Looper\core\services\Http;
+use Looper\core\models\Repository;
+use Looper\core\router\RouterManager;
+use Looper\core\controllers\AbstractController;
+use Looper\models\ExerciseState;
+use Looper\models\Status;
+
+class SerieController extends AbstractController
+{
+}
