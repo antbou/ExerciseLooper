727667a12705772ab4b3106d93fa20c904af1227
DELGADO Noah <Noah.DELGADO@cpnv.ch>
2021-12-07 14:13:45 +0100

    Add showAnswer methode in Seriecontroller and serie view

--- controllers/SerieController.php
+++ controllers/SerieController.php
@@ -2,14 +2,25 @@
 
 namespace Looper\controllers;
 
+use Looper\models\Serie;
+use Looper\models\Status;
 use Looper\models\Exercise;
+use Looper\core\forms\Field;
 use Looper\core\services\Http;
+use Looper\models\ExerciseState;
 use Looper\core\models\Repository;
+use Looper\core\forms\FormValidator;
 use Looper\core\router\RouterManager;
 use Looper\core\controllers\AbstractController;
-use Looper\models\ExerciseState;
-use Looper\models\Status;
 
 class SerieController extends AbstractController
 {
+
+    public function showAnswer(int $idExercise, int $idSerie)
+    {
+        $exercise = Repository::find($idExercise, Exercise::class);
+        $serie = Repository::find($idSerie, Serie::class);
+        if (empty($exercise) || empty($serie)) return Http::notFoundException();
+        return Http::response('serie/show', ['exercise' => $exercise, 'serie' => $serie]);
+    }
 }
