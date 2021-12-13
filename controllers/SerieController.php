d801c3396dce7a4697704c20d21c33416a88c57e
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-13 10:56:25 +0100

    Refactoring SerieController

--- controllers/SerieController.php
+++ controllers/SerieController.php
@@ -2,24 +2,19 @@
 
 namespace Looper\controllers;
 
-use Looper\models\Serie;
-use Looper\models\Status;
 use Looper\models\Exercise;
-use Looper\core\forms\Field;
 use Looper\core\services\Http;
-use Looper\models\ExerciseState;
 use Looper\core\models\Repository;
-use Looper\core\forms\FormValidator;
 use Looper\core\router\RouterManager;
 use Looper\core\controllers\AbstractController;
 
 class SerieController extends AbstractController
 {
 
-    public function showAnswer(int $idExercise, int $idSerie)
+    public function show(int $idExercise, int $idSerie)
     {
         $exercise = Repository::find($idExercise, Exercise::class);
-        $serie = Repository::find($idSerie, Serie::class);
+        $serie = ($exercise) ? $exercise->getSerieById($idSerie) : null;
         if (empty($exercise) || empty($serie)) return Http::notFoundException();
         return Http::response(
             'serie/show',
