585b271d8f0aad4add7aeea7e886fd94237579d2
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-13 10:40:19 +0100

    Refactoring ManageExerciseController

--- controllers/ManageExerciseController.php
+++ controllers/ManageExerciseController.php
@@ -5,19 +5,16 @@ namespace Looper\controllers;
 use Looper\models\Exercise;
 use Looper\core\services\Http;
 use Looper\core\models\Repository;
-use Looper\core\router\RouterManager;
 use Looper\core\controllers\AbstractController;
-use Looper\models\ExerciseState;
 use Looper\models\Status;
 
 class ManageExerciseController extends AbstractController
 {
-
     public function show()
     {
-        $buildedExercises = Repository::findAllWhere(Exercise::class, 'status_id', Repository::findAllWhere(Status::class, 'slug', 'UNDE')[0]->id);
-        $answeredExercises = Repository::findAllWhere(Exercise::class, 'status_id', Repository::findAllWhere(Status::class, 'slug', 'ANSW')[0]->id);
-        $closedExercises = Repository::findAllWhere(Exercise::class, 'status_id', Repository::findAllWhere(Status::class, 'slug', 'TERM')[0]->id);
+        $buildedExercises = Repository::findAllWhere(Exercise::class, 'status_id', Status::findBySlug('UNDE')->id);
+        $answeredExercises = Repository::findAllWhere(Exercise::class, 'status_id', Status::findBySlug('ANSW')->id);
+        $closedExercises = Repository::findAllWhere(Exercise::class, 'status_id', Status::findBySlug('TERM')->id);
         return Http::response('manage/index', ['buildedExercises' => $buildedExercises, 'answeredExercises' => $answeredExercises, 'closedExercises' => $closedExercises]);
     }
 }
