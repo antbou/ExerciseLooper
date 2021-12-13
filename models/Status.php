9e911eab6daf6c8991cb455ba4e163ab904d0cd2
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-13 10:37:34 +0100

    Add trait FindBySlug in Status Model

--- models/Status.php
+++ models/Status.php
@@ -3,9 +3,11 @@
 namespace Looper\models;
 
 use Looper\core\models\Model;
+use Looper\core\traits\FindBySlug;
 
 class Status extends Model
 {
+    use FindBySlug;
     public string $value;
     public string $slug;
 
