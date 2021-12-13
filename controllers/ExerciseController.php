54046e8b3b9c5088dfd623553031779d910fa6f3
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-13 09:25:43 +0100

    Add comments in ExerciseController

--- controllers/ExerciseController.php
+++ controllers/ExerciseController.php
@@ -14,6 +14,54 @@ use Looper\core\controllers\AbstractController;
 class ExerciseController extends AbstractController
 {
 
+    public function create()
+    {
+        $form = new FormValidator('exercise');
+        $form->addField(['title' => new Field('title', 'string', true)]);
+
+        if ($form->process() && $this->csrfValidator()) {
+            $exercise = Exercise::make([
+                'title' => $form->getFields()['title']->value,
+            ]);
+            if ($exercise->create()) return Http::redirectToRoute('CreateQuestion', ['idExercise' => $exercise->id]);
+        }
+
+        return Http::response('exercise/create', ['title' => Exercise::DEFAULTNAME], hasForm: true);
+    }
+
+    public function delete(int $id)
+    {
+        $exercise = Repository::find($id, Exercise::class);
+        if (empty($exercise)) return Http::notFoundException();
+        if (!$exercise->delete()) return Http::internalServerError();
+
+        return Http::responseApi(['route' => RouterManager::getRouter()->getUrl('ManageExercise', [])]);
+    }
+
+    /**
+     * Displays the answers given for an exercise
+     *
+     * @param integer $id
+     * @return void
+     */
+    public function results(int $id)
+    {
+        $exercise = Repository::find($id, Exercise::class);
+        if (empty($exercise)) return Http::notFoundException();
+
+        return Http::response(
+            'exercise/results',
+            ['exercise' => $exercise, 'link' => RouterManager::getRouter()->getUrl('ResultExercise', ['idExercise' => $exercise->id])]
+        );
+    }
+
+    /**
+     * Change the status of an exercise
+     *
+     * @param integer $id
+     * @param string $slug
+     * @return void
+     */
     public function status(int $id, string $slug)
     {
         $exercise = Repository::find($id, Exercise::class);
@@ -29,40 +77,5 @@ class ExerciseController extends AbstractController
         if (!$exercise->update()) return Http::internalServerError();
 
         return http::responseApi(['route' => RouterManager::getRouter()->getUrl('ManageExercise', [])]);
-    }
-
-    public function delete(int $id)
-    {
-        $exercise = Repository::find($id, Exercise::class);
-        if (empty($exercise)) return Http::notFoundException();
-        if (!$exercise->delete()) return Http::internalServerError();
-
-        return Http::responseApi(['route' => RouterManager::getRouter()->getUrl('ManageExercise', [])]);
-    }
-
-    public function results(int $id)
-    {
-        $exercise = Repository::find($id, Exercise::class);
-        if (empty($exercise)) return Http::notFoundException();
-
-        return Http::response(
-            'exercise/results',
-            ['exercise' => $exercise, 'link' => RouterManager::getRouter()->getUrl('ResultExercise', ['idExercise' => $exercise->id])]
-        );
-    }
-
-    public function create()
-    {
-        $form = new FormValidator('exercise');
-        $form->addField(['title' => new Field('title', 'string', true)]);
-
-        if ($form->process() && $this->csrfValidator()) {
-            $exercise = Exercise::make([
-                'title' => $form->getFields()['title']->value,
-            ]);
-            if ($exercise->create()) return Http::redirectToRoute('CreateQuestion', ['idExercise' => $exercise->id]);
-        }
-
-        return Http::response('exercise/create', ['title' => Exercise::DEFAULTNAME], hasForm: true);
     }
 }
