d2b8d1ddddf8f86a16f05dd9d5e30b27fc564d38
Anthony Bouillant <anthony.bouillant@cpnv.ch>
2021-12-13 11:20:52 +0100

    reorganize the order of the routes

--- config/routeConfig.php
+++ config/routeConfig.php
@@ -11,48 +11,6 @@ return [
         'Controller' => 'ExerciseController',
         'Method' => 'create'
     ],
-    'CreateQuestion' => [
-        'URI' => '/exercises/:idExercise/fields',
-        'Controller' => 'QuestionController',
-        'Method' => 'create'
-    ],
-    'TakeExercise' => [
-        'URI' => '/exercises/answering',
-        'Controller' => 'TakeExerciseController',
-        'Method' => 'showAllAnswer'
-    ],
-    'ShowAnswer' => [
-        'URI' => '/exercises/:idExercise/fulfillments/new',
-        'Controller' => 'TakeExerciseController',
-        'Method' => 'showAnswer'
-    ],
-    'SaveAnswer' => [
-        'URI' => '/exercises/:idExercise/fulfillments',
-        'Controller' => 'TakeExerciseController',
-        'Method' => 'saveAnswer'
-    ],
-    'DeleteQuestion' => [
-        'URI' => '/exercises/:idExercise/fields/:idQuestion',
-        'Controller' => 'QuestionController',
-        'Method' => 'delete',
-        'HttpMethod' => 'delete'
-    ],
-    'EditQuestion' => [
-        'URI' => '/exercises/:idExercise/fields/:idQuestion/edit',
-        'Controller' => 'QuestionController',
-        'Method' => 'edit',
-    ],
-    'StatusExercise' => [
-        'URI' => '/exercises/:idExercise/status/:slug',
-        'Controller' => 'ExerciseController',
-        'Method' => 'status',
-        'HttpMethod' => 'put'
-    ],
-    'ManageExercise' => [
-        'URI' => '/exercises',
-        'Controller' => 'ManageExerciseController',
-        'Method' => 'show',
-    ],
     'EditExercise' => [
         'URI' => '/exercises/:idExercise/edit',
         'Controller' => 'ExerciseController',
@@ -63,30 +21,71 @@ return [
         'Controller' => 'ExerciseController',
         'Method' => 'delete',
     ],
-    'EditAnswer' => [
-        'URI' => '/exercises/:idExercise/fulfillments/:idSerie',
-        'Controller' => 'TakeExerciseController',
-        'Method' => 'edit',
-    ],
-    'ShowAnswerFilled' => [
-        'URI' => '/exercises/:idExercise/fulfillments/:idSerie/edit',
-        'Controller' => 'TakeExerciseController',
-        'Method' => 'showAnswerFilled',
-    ],
-    'ShowSeriesAnswer' => [
-        'URI' => '/exercises/:idExercise/fulfillments/:idSerie/show',
-        'Controller' => 'SerieController',
-        'Method' => 'show',
+    'StatusExercise' => [
+        'URI' => '/exercises/:idExercise/status/:slug',
+        'Controller' => 'ExerciseController',
+        'Method' => 'status',
+        'HttpMethod' => 'put'
     ],
     'ResultExercise' => [
         'URI' => '/exercises/:idExercise/results',
         'Controller' => 'ExerciseController',
         'Method' => 'results',
     ],
+    'CreateQuestion' => [
+        'URI' => '/exercises/:idExercise/fields',
+        'Controller' => 'QuestionController',
+        'Method' => 'create'
+    ],
+    'EditQuestion' => [
+        'URI' => '/exercises/:idExercise/fields/:idQuestion/edit',
+        'Controller' => 'QuestionController',
+        'Method' => 'edit',
+    ],
+    'DeleteQuestion' => [
+        'URI' => '/exercises/:idExercise/fields/:idQuestion',
+        'Controller' => 'QuestionController',
+        'Method' => 'delete',
+        'HttpMethod' => 'delete'
+    ],
     'ResultsQuestion' => [
         'URI' => '/exercises/:idExercise/results/:idQuestion',
         'Controller' => 'QuestionController',
         'Method' => 'showAnswers',
     ],
-
+    'TakeExercise' => [
+        'URI' => '/exercises/answering',
+        'Controller' => 'TakeExerciseController',
+        'Method' => 'showExercises'
+    ],
+    'showQuestions' => [
+        'URI' => '/exercises/:idExercise/fulfillments/new',
+        'Controller' => 'TakeExerciseController',
+        'Method' => 'showQuestions'
+    ],
+    'SaveAnswer' => [
+        'URI' => '/exercises/:idExercise/fulfillments',
+        'Controller' => 'TakeExerciseController',
+        'Method' => 'saveAnswer'
+    ],
+    'EditAnswer' => [
+        'URI' => '/exercises/:idExercise/fulfillments/:idSerie',
+        'Controller' => 'TakeExerciseController',
+        'Method' => 'edit',
+    ],
+    'ShowAnswersFilled' => [
+        'URI' => '/exercises/:idExercise/fulfillments/:idSerie/edit',
+        'Controller' => 'TakeExerciseController',
+        'Method' => 'showAnswersFilled',
+    ],
+    'ManageExercise' => [
+        'URI' => '/exercises',
+        'Controller' => 'ManageExerciseController',
+        'Method' => 'show',
+    ],
+    'ShowSeriesAnswer' => [
+        'URI' => '/exercises/:idExercise/fulfillments/:idSerie/show',
+        'Controller' => 'SerieController',
+        'Method' => 'show',
+    ],
 ];
